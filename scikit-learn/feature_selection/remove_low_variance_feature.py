5a87710230b7d7352617c9afce9cd88f16ef769c
xaxiamx <xaxiamx@cn.ibm.com>
2018-08-25 21:06:20 +0800

    check-in examples 	new file:   scikit-learn/bagging_and_boosting/decision_tree_regression_with_adaboost.py 	new file:   scikit-learn/bagging_and_boosting/discrete_vs_real_adaboost.py 	new file:   scikit-learn/bagging_and_boosting/gradient_boosting_regression.py 	new file:   scikit-learn/bagging_and_boosting/gradient_boosting_regularization.py 	new file:   scikit-learn/bagging_and_boosting/grandient_boosting_out_of_bag_estimates.py 	new file:   scikit-learn/bagging_and_boosting/hashing_feature_transformation_using_totally_random_trees.py 	new file:   scikit-learn/bagging_and_boosting/manifold_learning_on_handwritten_digits.py 	new file:   scikit-learn/bagging_and_boosting/multi_class_adaboosted_decision_tree.py 	new file:   scikit-learn/bagging_and_boosting/oob_errors_for_random_forests.py 	new file:   scikit-learn/bagging_and_boosting/partial_dependence_plots.py 	new file:   scikit-learn/bagging_and_boosting/two_class_adaboost.py 	new file:   scikit-learn/feature_selection/classification_of_text_documents_using_spare_features.py 	new file:   scikit-learn/feature_selection/comparison_of_F_test_and_mutual_information.py 	new file:   scikit-learn/feature_selection/feature_selection_using_selectfrommodel_and_lassocv.py 	new file:   scikit-learn/feature_selection/recursive_feature_elimination.py 	new file:   scikit-learn/feature_selection/recursive_feature_elimination_with_cross_validation.py 	new file:   scikit-learn/feature_selection/remove_low_variance_feature.py 	new file:   scikit-learn/feature_selection/remove_other_feature_except_first_K_feature.py 	new file:   scikit-learn/feature_selection/univariate_feature_selection.py 	new file:   scikit-learn/half_supervised/decision_boundary_of_label_propagation_vs_svm_on_iris.py 	new file:   scikit-learn/half_supervised/label_propagation_digits_active_learning.py 	new file:   scikit-learn/half_supervised/label_propagation_learning_a_complex_structure.py 	new file:   scikit-learn/multilclass_and_multilabel/multilabel_classification.py

---
+++ scikit-learn/feature_selection/remove_low_variance_feature.py
@@ -0,0 +1,7 @@
+from sklearn.feature_selection import VarianceThreshold
+X = [[0, 0, 1], [0, 1, 0], [1, 0, 0], [0, 1, 1], [0, 1, 0], [0, 1, 1]]
+sel = VarianceThreshold(threshold=(.8 * (1 - .8)))
+X_sel = sel.fit_transform(X)
+
+print (X)
+print(X_sel)
